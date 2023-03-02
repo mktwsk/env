0fb649369e9a1182054a38c44073d58d41ddb3cb
wielebny <wielebny@k8s-master.ktwsk.net>
2023-03-02 04:28:39 +0100

    ah

--- doit.sh
+++ doit.sh
@@ -4,7 +4,22 @@ echo "Sprawdzam distro: "
 DISTRO=$(lsb_release -i | cut -f 2-)
 echo $DISTRO
 
+if [ "$DISTRO" = "Debian" ]; then
+	sudo apt update &&
+	sudo apt upgrade &&
+	sudo apt install aptitude &&
+	sudo aptitude update &&
+	sudo aptitude install zsh &&
+	sudo aptitude install vim &&
+	sudo aptitude install git &&
+	sudo aptitude install rsync
+else
+	echo "To nie moje distro"
+fi
+
+echo "Rsync .vim"
 rsync -a --info=progress2 --no-i-r ~/git/env/.vim ~/
+echo "Rsync .oh-my-zsh"
 rsync -a --info=progress2 --no-i-r ~/git/env/.oh-my-zsh ~/
 
 
@@ -13,10 +28,6 @@ cp  .zshrc ~/
 
 echo "\n Helptagi dla Vima"
 vim -u NONE -c "helptags fugitive/doc" -c q
-
-
-DISTRO=$(lsb_release -i | cut -f 2-)
-echo $DISTRO
 
 
 echo "Uczynił żem!"
